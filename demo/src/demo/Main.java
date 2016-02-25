41358a186e62af5535a9c76a33d4d9f665f6e6f7
Luis Angel Sanchez Jr. <sanzluisangel@icloud.com>
2016-02-25 15:22:42 -0600

    There you go

--- demo/src/demo/Main.java
+++ demo/src/demo/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("I'm understanding this.....I think.");
+		System.out.println("I'm understanding this.....I think, so you think so.");
 
 	}
 
